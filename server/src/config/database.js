0f05d8e5be5ca8185364c912444f0e3e6628ddf6
souz4s <psouza.1650@gmail.com>
2021-11-07 04:44:17 -0300

    changing password

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql");
 
 module.exports = class DatabaseConfig {
-  connectDatabase() {
-    const db = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      password: "root",
-      database: "tickets_register",
-    });
+    connectDatabase() {
+        const db = mysql.createPool({
+            host: "localhost",
+            user: "root",
+            password: "password",
+            database: "tickets_register",
+        });
 
-    return db;
-  }
+        return db;
+    }
 };
