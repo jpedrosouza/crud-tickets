4a0fce5d2bb4a292f836d5a6f6e9c07e29541281
João Pedro Souza <jsouza.1650@gmail.com>
2021-11-07 03:18:44 -0300

    created TicketsController class

---
+++ server/src/controllers/ticket_controller.js
@@ -0,0 +1,31 @@
+const databaseConfig = require("../config/database");
+
+module.exports = class TicketsController {
+  constructor() {
+    this.db = new databaseConfig().connectDatabase();
+  }
+
+  // Insert ticket in database.
+
+  async store(req, res) {
+    const evento = req.body.evento;
+    const artista = req.body.artista;
+    const local = req.body.local;
+    const descricao = req.body.descricao;
+    const data = req.body.data;
+    const preco = req.body.preco;
+    const url = req.body.url;
+
+    this.db.query(
+      "INSERT INTO tickets (evento, artista, local, descricao, data, preco, url) VALUES (?,?,?,?,?,?,?)",
+      [evento, artista, local, descricao, data, preco, url],
+      (err, response) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+
+        return res.status(200).send({ msg: "Evento registrado com sucesso" });
+      }
+    );
+  }
+};
